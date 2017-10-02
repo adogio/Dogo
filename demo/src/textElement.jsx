5384e5f03d35c9824f677a697d146de66824e2cf
Meng Wang <wm@wmpcxpy.com>
2017-10-02 01:00:59 -0500

    add different color of hover to textelement component

--- demo/src/textElement.jsx
+++ demo/src/textElement.jsx
@@ -5,12 +5,15 @@ import React, {
 class TextElement extends Component {
     bothColor = { transition: "0.2s all" }
     defaultColor = { color: "black" };
-    hoverColor = { color: "red" };
+    hoverColor = null;
+    hoverColorList = ["FF3939", "FF7139", "BAFF39", "FFB039", "39FFA3", "39A6FF", "3978FF", "4639FF", "9539FF", "D739FF", "FF39BD"];
 
     constructor(props) {
         super(props);
         this.handleHover = this.handleHover.bind(this);
         this.releaseHover = this.releaseHover.bind(this);
+        this.getHoverColor = this.getHoverColor.bind(this);
+        this.hoverColor = this.getHoverColor();
         this.state = {
             hover: false
         }
@@ -31,6 +34,12 @@ class TextElement extends Component {
                 points={this.props.points}
             >{this.props.char}</span>
         );
+    }
+
+    getHoverColor() {
+        let color = null;
+        color = this.hoverColorList[Math.floor(Math.random() * 1000) % this.hoverColorList.length];
+        return { color: "#" + color };
     }
 
     handleHover() {
